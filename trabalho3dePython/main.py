799f98beb8043b39b91b180d42387cc55bc4d143
SuitingPie52 <126472855+SuitingPie52@users.noreply.github.com>
2023-11-20 10:35:43 -0300

    Update main.py

--- trabalho3dePython/main.py
+++ trabalho3dePython/main.py
@@ -30,16 +30,13 @@ def menu():
               "|        BEM-VINDO, O QUE VOCÊ DESEJA FAZER?       |\n"
               "|==================================================|\n"
               "| 1 - Adicionar cliente                            |\n"
-              "| 2 - Passar informações de um cliente pra .txt    |\n"
-              "| 3 - Imprimir informações de todos os clientes    |\n"
-              "| 4 - Criar conta para um cliente                  |\n"
-              "| 5 - Passar informações de uma conta pra .txt     |\n"
-              "| 6 - Imprimir informações de todas as contas      |\n"
-              "| 7 - Depositar na conta                           |\n"
-              "| 8 - Sacar da conta                               |\n"
-              "| 9 - Transferir de uma conta para outra           |\n"
-              "| 10 - Passar informações do histórico para .txt   |\n"
-              "| 11 - Imprimir histórico                          |\n"
+              "| 2 - Imprimir informações de todos os clientes    |\n"
+              "| 3 - Criar conta para um cliente                  |\n"
+              "| 4 - Imprimir informações de todas as contas      |\n"
+              "| 5 - Depositar na conta                           |\n"
+              "| 6 - Sacar da conta                               |\n"
+              "| 7 - Transferir de uma conta para outra           |\n"
+              "| 8 - Imprimir histórico                           |\n"
               "|==================================================|\n"
               "| -1 - Encerrar programa                           |\n"
               "|==================================================|\n\n")
@@ -56,21 +53,9 @@ def menu():
 
         elif opcao == 2:
 
-            cpf = input("Qual será o CPF do cliente que você deseja escrever em .txt? ")
-
-            cliente_desejado = encontrar_pelo_cpf(cpf, clientes)
-
-            if cliente_desejado:
-                cliente_desejado.gravar_arq()
-                print("Os dados foram escritos!")
-            else:
-                print("Cliente não encontrado.")
-
-        elif opcao == 3:
-
             imprimir_todos_conjunto(clientes)
 
-        elif opcao == 4:
+        elif opcao == 3:
 
             cpf = input("Você deseja abrir uma conta para qual cliente? (Informe o CPF dele) ")
             numero_conta = input("Qual será o número de identificação dessa conta? ")
@@ -79,24 +64,11 @@ def menu():
             contas.append(Conta(cpf, numero_conta, saldo))
             print("Conta adicionado!")
 
-        elif opcao == 5:
-
-            cpf = input("Qual é o CPF identificador dessa conta que você deseja escrever em .txt? ")
-
-            conta_desejada = encontrar_pelo_cpf(cpf, contas)
-
-            if conta_desejada:
-                conta_desejada.gravar_arq()
-                print("Os dados foram escritos!")
-
-            else:
-                print("Conta não encontrada.")
-
-        elif opcao == 6:
+        elif opcao == 4:
 
             imprimir_todos_conjunto(contas)
 
-        elif opcao == 7:
+        elif opcao == 5:
 
             cpf = input("Qual é o CPF identificador da conta em que você deseja depositar? ")
             valor = int(input("Qual é o valor que você deseja depositar? "))
@@ -111,7 +83,7 @@ def menu():
             else:
                 print("Conta não encontrada.")
 
-        elif opcao == 8:
+        elif opcao == 6:
 
             cpf = input("Qual é o CPF identificador da conta em que você quer sacar? ")
             valor = int(input("Qual é o valor que você deseja sacar? "))
@@ -129,7 +101,7 @@ def menu():
             else:
                 print("Conta não encontrada.")
 
-        elif opcao == 9:
+        elif opcao == 7:
 
             cpf1 = input("Você deseja transferir de qual conta? (Informe o CPF) ")
             cpf2 = input("Para qual? (Informe o cpf) ")
@@ -146,12 +118,19 @@ def menu():
             else:
                 print("Erro ao passar o CPF das contas.")
 
-        elif opcao == 10:
-            h.gravar_arq()
-            print("Os dados foram gravados!")
+        elif opcao == 8:
 
-        elif opcao == 11:
             h.imprimir()
+
+    for i in clientes:
+
+        i.gravar_arq()
+
+    for i in contas:
+
+        i.gravar_arq()
+
+    h.gravar_arq()
 
 
 menu()
