ae77fd69bde00a80a48d98f60467427f00c93120
SuitingPie52 <126472855+SuitingPie52@users.noreply.github.com>
2023-11-20 11:27:06 -0300

    Update main.py

--- trabalho3dePython/main.py
+++ trabalho3dePython/main.py
@@ -19,6 +19,15 @@ def encontrar_pelo_cpf(cpf, conjunto_classes):
 
 
 def menu():
+    """
+    abrindo o arquivo com w apenas para limpar os dados anteriors, já que eles são gravados com a
+    """
+
+    with open("Contas.txt", "w") as arquivo:
+        arquivo.write("")
+
+    with open("Clientes.txt", "w") as arquivo:
+        arquivo.write("")
 
     opcao = 0
     clientes = []
@@ -29,14 +38,13 @@ def menu():
         print("\n|==================================================|\n"
               "|        BEM-VINDO, O QUE VOCÊ DESEJA FAZER?       |\n"
               "|==================================================|\n"
-              "| 1 - Adicionar cliente                            |\n"
+              "| 1 - Adicionar cliente e conta                    |\n"
               "| 2 - Imprimir informações de todos os clientes    |\n"
-              "| 3 - Criar conta para um cliente                  |\n"
-              "| 4 - Imprimir informações de todas as contas      |\n"
-              "| 5 - Depositar na conta                           |\n"
-              "| 6 - Sacar da conta                               |\n"
-              "| 7 - Transferir de uma conta para outra           |\n"
-              "| 8 - Imprimir histórico                           |\n"
+              "| 3 - Imprimir informações de todas as contas      |\n"
+              "| 4 - Depositar na conta                           |\n"
+              "| 5 - Sacar da conta                               |\n"
+              "| 6 - Transferir de uma conta para outra           |\n"
+              "| 7 - Imprimir histórico                           |\n"
               "|==================================================|\n"
               "| -1 - Encerrar programa                           |\n"
               "|==================================================|\n\n")
@@ -47,9 +55,14 @@ def menu():
 
             cpf = input("Qual será o CPF desse cliente? ")
             nome = input("Qual será o nome desse cliente? ")
+            numero_conta = input("Qual será o número de identificação da conta? ")
+            saldo = int(input("Qual será o saldo inicial da conta? "))
 
             clientes.append(Cliente(cpf, nome))
-            print("Cliente adicionado!")
+            contas.append(Conta(cpf, numero_conta, saldo))
+            print("Cliente e conta adicionados!")
+
+            h = h.atualizar(f"Abertura: {str(cpf)}")
 
         elif opcao == 2:
 
@@ -57,18 +70,9 @@ def menu():
 
         elif opcao == 3:
 
-            cpf = input("Você deseja abrir uma conta para qual cliente? (Informe o CPF dele) ")
-            numero_conta = input("Qual será o número de identificação dessa conta? ")
-            saldo = int(input("Qual será o saldo inicial dessa conta? "))
-
-            contas.append(Conta(cpf, numero_conta, saldo))
-            print("Conta adicionado!")
-
-        elif opcao == 4:
-
             imprimir_todos_conjunto(contas)
 
-        elif opcao == 5:
+        elif opcao == 4:
 
             cpf = input("Qual é o CPF identificador da conta em que você deseja depositar? ")
             valor = int(input("Qual é o valor que você deseja depositar? "))
@@ -83,7 +87,7 @@ def menu():
             else:
                 print("Conta não encontrada.")
 
-        elif opcao == 6:
+        elif opcao == 5:
 
             cpf = input("Qual é o CPF identificador da conta em que você quer sacar? ")
             valor = int(input("Qual é o valor que você deseja sacar? "))
@@ -101,7 +105,7 @@ def menu():
             else:
                 print("Conta não encontrada.")
 
-        elif opcao == 7:
+        elif opcao == 6:
 
             cpf1 = input("Você deseja transferir de qual conta? (Informe o CPF) ")
             cpf2 = input("Para qual? (Informe o cpf) ")
@@ -118,7 +122,7 @@ def menu():
             else:
                 print("Erro ao passar o CPF das contas.")
 
-        elif opcao == 8:
+        elif opcao == 7:
 
             h.imprimir()
 
